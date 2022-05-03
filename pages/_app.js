1170c032e8062853882beaca4a13edc9d2434bdf
Amirhossein Balagar <amirhossinbalagar@gmail.com>
2022-05-03 10:42:49 +0430

    static routing

---
+++ pages/_app.js
@@ -0,0 +1,11 @@
+import Head from 'next/head';
+
+import '../styles/globals.css';
+
+function MyApp({ Component, pageProps }) {
+  return (
+    <Component {...pageProps} />
+  );
+}
+
+export default MyApp;
