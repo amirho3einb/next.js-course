e654b35fcc3cca77b5382462e35fb25c515e5724
Amirhossein Balagar <amirhossinbalagar@gmail.com>
2022-05-03 16:04:52 +0430

    A Different Way Of Setting Link Hrefs

--- pages/clients/index.js
+++ pages/clients/index.js
@@ -9,9 +9,21 @@ const ClientsPage = () => {
   return (
     <div>
       <h1>The Clients Page</h1>
-      {clients.map((client) => (
+      {/* {clients.map((client) => (
         <li key={client.id}>
           <Link href={`/clients/${client.id}`}>{client.name}</Link>
+        </li>
+      ))} */}
+      {clients.map((client) => (
+        <li key={client.id}>
+          <Link
+            href={{
+              pathname: "/clients/[id]",
+              query: { id: client.id },
+            }}
+          >
+            {client.name}
+          </Link>
         </li>
       ))}
     </div>
