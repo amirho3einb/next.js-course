a06869f53b995042fc0668d362f882b5458a30b6
Amirhossein Balagar <amirhossinbalagar@gmail.com>
2022-05-03 15:56:41 +0430

    Navigating To Dynamic Routes

--- pages/clients/[id]/index.js
+++ pages/clients/[id]/index.js
@@ -1,4 +1,8 @@
+import { useRouter } from "next/router";
+
 const ClientProjectsPage = () => {
+  const router = useRouter();
+  console.log(router.query);
   return (
     <div>
       <h1>The Projects Of a Given Client</h1>
--- pages/clients/index.js
+++ pages/clients/index.js
@@ -1,7 +1,19 @@
+import Link from "next/link";
+
 const ClientsPage = () => {
+  const clients = [
+    { id: "alimp", name: "Ali Mohammadipour" },
+    { id: "swmg", name: "Sama Moazemi" },
+    { id: "bala", name: "Amirhossein Balagar" },
+  ];
   return (
     <div>
-      <h>The Clients Page</h>
+      <h1>The Clients Page</h1>
+      {clients.map((client) => (
+        <li key={client.id}>
+          <Link href={`/clients/${client.id}`}>{client.name}</Link>
+        </li>
+      ))}
     </div>
   );
 };
