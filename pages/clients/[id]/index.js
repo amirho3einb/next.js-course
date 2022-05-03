155417aaa9bb64de7eed6ee4ebf32b464c017d9a
Amirhossein Balagar <amirhossinbalagar@gmail.com>
2022-05-03 16:11:38 +0430

    Navigating Programmatically

--- pages/clients/[id]/index.js
+++ pages/clients/[id]/index.js
@@ -3,9 +3,13 @@ import { useRouter } from "next/router";
 const ClientProjectsPage = () => {
   const router = useRouter();
   console.log(router.query);
+  const loadProjectHandler = () => {
+    router.push("/clients/max/projectA");
+  };
   return (
     <div>
       <h1>The Projects Of a Given Client</h1>
+      <button onClick={loadProjectHandler}>load project A</button>
     </div>
   );
 };
