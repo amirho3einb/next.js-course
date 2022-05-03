2d3226cd725a7f3dec09abfe6a36681722a90a1d
Amirhossein Balagar <amirhossinbalagar@gmail.com>
2022-05-03 13:46:10 +0430

    Building Nested Dynamic Routes & Paths

---
+++ pages/clients/[id]/index.js
@@ -0,0 +1,9 @@
+const ClientProjectsPage = () => {
+  return (
+    <div>
+      <h1>The Projects Of a Given Client</h1>
+    </div>
+  );
+};
+
+export default ClientProjectsPage;
---
+++ pages/clients/index.js
@@ -0,0 +1,9 @@
+const ClientsPage = () => {
+  return (
+    <div>
+      <h>The Clients Page</h>
+    </div>
+  );
+};
+
+export default ClientsPage;
